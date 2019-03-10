c054ee6a0fea06df2856255159f10d3b2dd69dcd
Vladimir Novoselskiy <vladimirnovoselskiy@vladimirs-macbook-pro.local>
2019-03-10 10:32:35 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Vladimir Novoselskiy on 3/10/19.
+//  Copyright © 2019 Maria Novoselskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
